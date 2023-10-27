fc708a896e53a0965c9b934e762ee1a169d8803a
Uday Kiran Kolluru <113407692+uday-kiran-k@users.noreply.github.com>
2023-10-27 12:45:48 -0400

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -4,6 +4,5 @@ module.exports = {
     '@semantic-release/commit-analyzer', // Analyzes commits to determine version bump
     '@semantic-release/release-notes-generator', // Generates release notes
     '@semantic-release/github', // Creates a GitHub release
-    '@semantic-release/git' // Commits version changes back to the repository
   ]
 };
