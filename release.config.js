44546a57d26966ce9dc5ff77deed0de3bed2b0f0
Uday Kiran Kolluru <113407692+uday-kiran-k@users.noreply.github.com>
2023-10-27 12:32:31 -0400

    feat: Update release.config.js

--- release.config.js
+++ release.config.js
@@ -3,8 +3,6 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer', // Analyzes commits to determine version bump
     '@semantic-release/release-notes-generator', // Generates release notes
-    '@semantic-release/changelog', // Updates the CHANGELOG.md file
-    '@semantic-release/npm', // Publishes the package to npm
     '@semantic-release/github', // Creates a GitHub release
     '@semantic-release/git' // Commits version changes back to the repository
   ]
